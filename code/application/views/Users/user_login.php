2053f1c7b973b52febe97f92ed1b3421b6545a61
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-04-30 22:12:07 +0900

    fixed spacing, added comments

--- code/application/views/Users/user_login.php
+++ code/application/views/Users/user_login.php
@@ -13,66 +13,70 @@
     <link rel="stylesheet" href="<?php base_url() ?>/assets/stylesheets/user_login.css">
     <!-- fontawesome cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    
 </head>
 <body>
+
     <!-- Main container -->
     <div class="container-fluid mx-auto p-0">
         <!-- Navbar -->
         <header class="p-3 bg-dark text-white">
             <div class="container-fluid">
-              <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="<?php base_url(); ?>" class="d-flex align-items-center mb-lg-0 text-white text-decoration-none">
-                  <img src="<?php base_url() ?>/assets/images/cover.png" class="" width="150px" >
-                </a>
-          
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <!-- <li><a href="#" class="nav-link px-2 text-white">Orders</a></li>
-                  <li><a href="#" class="nav-link px-2 text-white">Products</a></li> -->
-                </ul>
-          
-                <div class="text-end">
-                    <a href="login" type="button" class="btn btn-outline-light me-2">Login</a>
-                    <a href="register" type="button" class="btn btn-warning">Sign-up</a>
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="<?php base_url(); ?>" class="d-flex align-items-center mb-lg-0 text-white text-decoration-none">
+                        <img src="<?php base_url() ?>/assets/images/cover.png" class="" width="150px" >
+                    </a>
+            
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <!-- <li><a href="#" class="nav-link px-2 text-white">Orders</a></li>
+                        <li><a href="#" class="nav-link px-2 text-white">Products</a></li> -->
+                    </ul>
+            
+                    <div class="text-end">
+                        <a href="login" type="button" class="btn btn-outline-light me-2">Login</a>
+                        <a href="register" type="button" class="btn btn-warning">Sign-up</a>
+                    </div>
                 </div>
-              </div>
             </div>
         </header>
           <!-- Navbar End -->
 
-
+         <!-- CONTAINER -->
         <div class="position-relative overflow-hidden bg-light banner-bgs">
-
+            <!-- LOGIN FORM -->
             <div class="container col-xl-10 col-xxl-8 px-4 py-1 mt-3 hero-style">
                 <div class="row align-items-center g-5 py-5">
-                  <div class="col-lg-7 text-center text-lg-start">
-                    <h1 class="display-4 fw-bold lh-1 mb-3">Welcome to Shoepify!</h1>
-                    <p class="col-lg-10 fs-4">Login to start viewing great deals!</p>
-                  </div>
-                  <div class="col-10 mx-auto col-lg-5">
-                    <form class="p-5 border rounded-3 bg-light">
-                      <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
-                      </div>
-                      <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                      </div>
-                      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
-                      <hr class="my-4">
-                      <p class="mb-0">Dont have an account?<a href="registration_page.html">Register Now!</a></p>
-                      
-                    </form>
-                  </div>
+                    <div class="col-lg-7 text-center text-lg-start">
+                        <h1 class="display-4 fw-bold lh-1 mb-3">Welcome to Shoepify!</h1>
+                        <p class="col-lg-10 fs-4">Login to start viewing great deals!</p>
+                    </div>
+                    <div class="col-10 mx-auto col-lg-5">
+                        <form class="p-5 border rounded-3 bg-light">
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                        <hr class="my-4">
+                        <p class="mb-0">Dont have an account?<a href="registration_page.html">Register Now!</a></p>
+                        </form>
+                    </div>
                 </div>
-              </div>
+            </div>
+            <!-- END LOGIN FORM -->
            
-            
+            <!-- footer -->
             <footer class="container-fluid mt-2 d-flex justify-content-center align-items-center">
                 <p class="text-center">&copy; 2021 Shoepify, Inc. &middot;</p>
             </footer>
-        </div>
+            <!-- END FOOTER -->
 
+        </div>
+        <!-- CONTAINER END -->
         
     </div>
     <!-- main container end -->
