7626b77c1bad0afbc55d004469261f9c40363d99
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-04-30 20:59:56 +0900

    user login page

---
+++ code/application/views/Users/user_login.php
@@ -0,0 +1,83 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- Personal CSS -->
+    <link rel="stylesheet" href="<?php base_url() ?>/assets/stylesheets/user_login.css">
+    <!-- fontawesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+</head>
+<body>
+    <!-- Main container -->
+    <div class="container-fluid mx-auto p-0">
+        <!-- Navbar -->
+        <header class="p-3 bg-dark text-white">
+            <div class="container-fluid">
+              <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="index.html" class="d-flex align-items-center mb-lg-0 text-white text-decoration-none">
+                  <img src="<?php base_url() ?>/assets/images/cover.png" class="" width="150px" >
+                </a>
+          
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <!-- <li><a href="#" class="nav-link px-2 text-white">Orders</a></li>
+                  <li><a href="#" class="nav-link px-2 text-white">Products</a></li> -->
+                </ul>
+          
+                <div class="text-end">
+                    <a href="users/login_page" type="button" class="btn btn-outline-light me-2">Login</a>
+                    <a href="users/registration_page" type="button" class="btn btn-warning">Sign-up</a>
+                </div>
+              </div>
+            </div>
+        </header>
+          <!-- Navbar End -->
+
+
+        <div class="position-relative overflow-hidden bg-light banner-bgs">
+
+            <div class="container col-xl-10 col-xxl-8 px-4 py-1 mt-3 hero-style">
+                <div class="row align-items-center g-5 py-5">
+                  <div class="col-lg-7 text-center text-lg-start">
+                    <h1 class="display-4 fw-bold lh-1 mb-3">Welcome to Shoepify!</h1>
+                    <p class="col-lg-10 fs-4">Login to start viewing great deals!</p>
+                  </div>
+                  <div class="col-10 mx-auto col-lg-5">
+                    <form class="p-5 border rounded-3 bg-light">
+                      <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                      </div>
+                      <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                      </div>
+                      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                      <hr class="my-4">
+                      <p class="mb-0">Dont have an account?<a href="registration_page.html">Register Now!</a></p>
+                      
+                    </form>
+                  </div>
+                </div>
+              </div>
+           
+            
+            <footer class="container-fluid mt-2 d-flex justify-content-center align-items-center">
+                <p class="text-center">&copy; 2021 Shoepify, Inc. &middot;</p>
+            </footer>
+        </div>
+
+        
+    </div>
+    <!-- main container end -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
+</html>
