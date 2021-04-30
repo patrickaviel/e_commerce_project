6c0b56c72ea7511291ccc4489b989bf6c95114f1
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-01 04:47:59 +0900

    whitespaces

--- code/application/views/index.php
+++ code/application/views/index.php
@@ -17,20 +17,20 @@
         <!-- Navbar -->
         <header class="p-3 bg-dark text-white">
             <div class="container-fluid">
-              <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="<?php base_url(); ?>" class="d-flex align-items-center mb-lg-0 text-white text-decoration-none">
-                  <img src="<?php base_url() ?>/assets/images/cover.png" class="" width="150px" >
-                </a>
-          
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                  <!-- <li><a href="#" class="nav-link px-2 text-white">Products</a></li> -->
-                </ul>
-          
-                <div class="text-end">
-                  <a href="login" type="button" class="btn btn-outline-light me-2">Login</a>
-                  <a href="register" type="button" class="btn btn-warning">Sign-up</a>
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="<?php base_url(); ?>" class="d-flex align-items-center mb-lg-0 text-white text-decoration-none">
+                        <img src="<?php base_url() ?>/assets/images/cover.png" class="" width="150px" >
+                    </a>
+            
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <!-- <li><a href="#" class="nav-link px-2 text-white">Products</a></li> -->
+                    </ul>
+            
+                    <div class="text-end">
+                        <a href="login" type="button" class="btn btn-outline-light me-2">Login</a>
+                        <a href="register" type="button" class="btn btn-warning">Sign-up</a>
+                    </div>
                 </div>
-              </div>
             </div>
         </header>
           <!-- Navbar End -->
