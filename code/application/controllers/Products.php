ec96979c2cd57dddd19dea85c1535fb1fd86efab
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-01 04:46:58 +0900

    added logout

--- code/application/controllers/Products.php
+++ code/application/controllers/Products.php
@@ -2,7 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Products extends CI_Controller {
+
     public function products_page() {
         $this->load->view('Products/products_page');
     }
+    
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect(base_url());   
+    }
+
 }
