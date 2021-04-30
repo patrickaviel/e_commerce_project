8b6fc37d232d997c221cb8d2153f8511287d8693
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-04-30 22:11:46 +0900

    modified route

--- code/application/config/routes.php
+++ code/application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'main';
 
 $route['login'] = 'users/login_page';
-$route['register'] = 'users/register_page';
+$route['register'] = 'users/registration_page';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
