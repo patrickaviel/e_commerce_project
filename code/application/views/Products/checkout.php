5b268f194858edd092170944525cb4d61688a733
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-06 16:24:30 +0900

    display cart items

--- code/application/views/Products/checkout.php
+++ code/application/views/Products/checkout.php
@@ -70,38 +70,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <span class="badge bg-primary rounded-pill"><?=count($this->cart->contents())?></span>
                         </h4>
                         <ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                            <h6 class="my-0">Product name</h6>
-                            <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$12</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                            <h6 class="my-0">Second product</h6>
-                            <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$8</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                            <h6 class="my-0">Third item</h6>
-                            <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$5</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between bg-light">
-                            <div class="text-success">
-                            <h6 class="my-0">Promo code</h6>
-                            <small>EXAMPLECODE</small>
-                            </div>
-                            <span class="text-success">−$5</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between">
-                            <span>Total (USD)</span>
-                            <strong>$20</strong>
-                        </li>
+<?php                   $total = 0;
+                        foreach($mycart as $item):          
+                            $total = $total + $item['qty'] * $item['price'];?>                        
+                            <li class="list-group-item d-flex justify-content-between lh-sm">
+                                <div>
+                                    <h6 class="my-0"><?=$item['name']?></h6>
+                                    <small class="text-muted">Qty: <?=$item['qty']?></small>
+                                </div>
+                                <span class="text-muted">₱<?=number_format($item['qty'] * $item['price'],2)?></span>
+                            </li>
+<?php                   endforeach;                         ?>
+                            <li class="list-group-item d-flex justify-content-between">
+                                <span>Total (PHP)</span>
+                                <strong>₱<?=number_format($total,2)?></strong>
+                            </li>
                         </ul>
                     </div>
                     
