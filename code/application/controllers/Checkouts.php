e5bbdc938ffa4db084beb4fa65a15ab26808e696
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-06 16:17:52 +0900

    created checkout model

---
+++ code/application/controllers/Checkouts.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Checkouts extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        // load Order_Model
+        $this->load->model('Order_Model');
+    }
+     
+	public function create_order()
+	{
+        // get user id from input
+		$user_id = $this->input->post('user_id');
+        // get all form data from post
+        $form_data = $this->input->post();
+        //  save and retreive billing id
+        $bill_id = $this->Order_Model->save_ship_info($form_data);
+        // save and retreive shipping id
+        $ship_id = $this->Order_Model->save_bill_info($form_data);
+        // get total from post
+        $total = $this->security->xss_clean($this->input->post('total'));
+        // set status to 'Order in Process'
+        $status = 'Order in Process';
+        // save and retreive order id
+        $order_id = $this->Order_Model->save_order($user_id,$bill_id,$ship_id,$total,$status);
+        // save order details to database
+        $this->Order_Model->save_order_details($order_id,$item_id,$total,$qty);
+        // load items from cart
+        $mycart = $this->cart->contents();
+        // insert items from cart using loop
+        foreach ($mycart as $item) {
+            $order_detail = array(
+                'order_id' => $order_id,
+                'product_id' => $item['id'],
+                'quantity' => $item['qty'],
+                'price' => $item['price'],
+                'total' => $this->security->xss_clean($this->input->post('total'))
+                );
+            // insert item
+            $this->Order_Model->update_item_inventory($item['id'],$item['qty']);
+            $order_detail_id = $this->product_model->order_detail($order_detail);
+        }
+        // destroy cart after inserting items
+        // $this->cart->destroy();
+        // displaying message after succcessfully inserting items
+        $this->session->set_flashdata('success_purchase', "<h3>Your order was placed successfully!</h3>");
+
+        $this->load->view();
+        echo '<pre>';
+        var_dump($form_data);
+        echo '<pre>';
+	}
+
+}
