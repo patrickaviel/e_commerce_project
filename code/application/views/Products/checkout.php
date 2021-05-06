6e73a3f0c8e70952f324b2d0cd37bb0db5046aab
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-06 16:23:02 +0900

    added cart count

--- code/application/views/Products/checkout.php
+++ code/application/views/Products/checkout.php
@@ -67,7 +67,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <div class="col-md-5 col-lg-4 order-md-last">
                         <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-primary">Your cart</span>
-                        <span class="badge bg-primary rounded-pill">3</span>
+                        <span class="badge bg-primary rounded-pill"><?=count($this->cart->contents())?></span>
                         </h4>
                         <ul class="list-group mb-3">
                         <li class="list-group-item d-flex justify-content-between lh-sm">
