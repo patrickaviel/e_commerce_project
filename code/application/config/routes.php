8a9a63779a3d985cfd9884bfe402e5ef0c32e30d
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-08 01:12:32 +0800

    fixed whitespaces

--- code/application/config/routes.php
+++ code/application/config/routes.php
@@ -1,25 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller']    = 'main';
+$route['default_controller']            = 'main';
 
-$route['login']                 = 'users/login_page';
-$route['register']              = 'users/registration_page';
+$route['login']                         = 'users/login_page';
+$route['register']                      = 'users/registration_page';
 
-$route['admin/login']           = 'admins/admin_login_page';
-$route['admin/register']        = 'admins/admin_registration_page';
-$route['admin/dashboard']       = 'admins/admin_dashboard';
-$route['admin/products']        = 'admins/admin_products';
-$route['admin/brands']          = 'admins/admin_brands';
-$route['admin/users']           = 'admins/admin_users';
+$route['admin/login']                   = 'admins/admin_login_page';
+$route['admin/register']                = 'admins/admin_registration_page';
+$route['admin/dashboard']               = 'admins/admin_dashboard';
+$route['admin/products']                = '/admins/admin_products';
+$route['admin/brands']                  = 'admins/admin_brands';
+$route['admin/users']                   = 'admins/admin_users';
 
-$route['logout']                = 'users/logout';
-
-// $route['products']              = 'products/products_page';
+$route['logout']                        = 'users/logout';
 
 // STRIPE API
-$route['make-stripe-payment'] = "StripePaymentController";
-$route['handleStripePayment']['post'] = "StripePaymentController/handlePayment";
+$route['make-stripe-payment']           = "StripePaymentController";
+$route['handleStripePayment']['post']   = "StripePaymentController/handlePayment";
 
 $route['404_override']          = '';
 $route['translate_uri_dashes']  = FALSE;
