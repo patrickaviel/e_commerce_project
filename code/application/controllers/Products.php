23a32ca5bdde1fc523db16c0bea99a39c8137b73
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-08 01:26:00 +0800

    redirect and whitespaces

--- code/application/controllers/Products.php
+++ code/application/controllers/Products.php
@@ -23,6 +23,8 @@ class Products extends CI_Controller {
             'total_rows'    => $this->Product_Model->count_items($where)
         ]);
         $data['pagination'] = $this->pagination->create_links();
+        $data['categories'] = $this->Product_Model->get_all_categories_count();
+        $data['item_count'] = $this->Product_Model->count_all_items();
         $this->load->view('Products/products_page',$data);
     }
 
@@ -49,7 +51,6 @@ class Products extends CI_Controller {
     public function add_category(){
         $this->form_validation
             ->set_rules('category','Category','required|trim|min_length[3]|is_unique[categories.category]');
-
         if($this->form_validation->run()==FALSE){
             $this->load->view('admins/admin_brands');
         }else{
@@ -68,82 +69,57 @@ class Products extends CI_Controller {
             ->set_rules('category','Category','required|trim')
             ->set_rules('quantity','Quantity','required|numeric')
             ->set_rules('price','Price','required|trim|greater_than[0]');
-
         if($this->form_validation->run()==FALSE){
-
-            // var_dump(validation_errors());
-            // echo "error";
-            $data['brands'] = $this->Product_Model->get_all_brands();
-            $data['categories'] = $this->Product_Model->get_all_categories();
-            $data['items'] = $this->Product_Model->get_all_items();
-            $this->session->set_flashdata('error', "Error adding new item!");
-            $this->load->view('Admins/admin_products',$data);
+            redirect('admin/products');
         }else{
             $ori_filename = $_FILES['image']['name'];
-            
             $new_name = time()."".str_replace(' ', '-', $ori_filename);
             $config = [
                 'upload_path'       =>      './product_images/',
                 'allowed_types'     =>      'gif|jpg|png',
                 'file_name'         =>      $new_name,
             ];
-
             $this->load->library('upload', $config);
-
-            if ( ! $this->upload->do_upload('image'))
-            {
-                    $image_error = $this->upload->display_errors();
-                    $data['brands'] = $this->Product_Model->get_all_brands();
-                    $data['categories'] = $this->Product_Model->get_all_categories();
-                    $data['items'] = $this->Product_Model->get_all_items();
-                    $this->session->set_flashdata('image_error', $image_error);
-                    $this->load->view('Admins/admin_products', $data);
-            }
-            else
-            {
-                    $image_filename = $this->upload->data('file_name');
-                    $form_data = $this->input->post();
-                    // var_dump($image_filename);
-                    // var_dump($form_data);
-                    $this->Product_Model->add_item($form_data,$image_filename);
-
-                    $data['brands'] = $this->Product_Model->get_all_brands();
-                    $data['categories'] = $this->Product_Model->get_all_categories();
-                    $data['items'] = $this->Product_Model->get_all_items();
-                    $this->session->set_flashdata('success', "Successfully added new item!");
-                    $this->load->view('Admins/admin_products',$data);
+            if ( ! $this->upload->do_upload('image')) {
+                $image_error = $this->upload->display_errors();
+                $this->session->set_flashdata('image_error', $image_error);
+                redirect('admin/products');
+            } else {
+                $image_filename = $this->upload->data('file_name');
+                $form_data = $this->input->post();
+                $this->Product_Model->add_item($form_data,$image_filename);
+                $this->session->set_flashdata('success', "Successfully added new item!");
+                redirect('admin/products');
             }
         } 
     }
 
     public function add_to_cart() {
 		$product_id = $this->input->post('prod_id',TRUE);
-		// $product_name = "Hello";
         $product_name = $this->input->post('name',TRUE);
 		$product_qty = $this->input->post('quantity',TRUE);
 		$product_price = $this->input->post('price',TRUE);
-		
 		$prod_list = array(
 			'id'    => $product_id,
 			'name'  => $product_name,
 			'price' => $product_price,
 			'qty'  	=> $product_qty
 		 );
-
-        // var_dump($prod_list);
-        
 		$this->cart->insert($prod_list);
-        // var_dump($this->cart->contents());
         $this->session->set_flashdata('success', "Added to cart!");
 		redirect('products/item_page/'.$product_id);
 	}
 
-    public function checkout(){
-        $data['mycart'] = $this->cart->contents();
-        $this->load->view('Products/checkout_page',$data);
+    public function checkout() {
+        if(is_null($this->session->userdata('user_id'))){
+            redirect('login');
+        } else {
+            $data['mycart'] = $this->cart->contents();
+            $this->load->view('Products/checkout_page',$data);
+        }
     }
 
-    public function remove_cart_item($id){
+    public function remove_cart_item($id) {
         $data = $this->cart->contents();
 		$data[$id]['qty'] = 0;
 		$this->cart->update($data);
