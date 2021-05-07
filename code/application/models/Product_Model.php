2b98c5fc1232ac0644eef8c9612cf4d1e761b467
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-05-08 01:27:57 +0800

    fixed queries

--- code/application/models/Product_Model.php
+++ code/application/models/Product_Model.php
@@ -5,33 +5,35 @@ class Product_Model extends CI_Model {
 
     // load all brands from the table brands
     function get_all_brands() {
-        $query = "Select * from brands";
+        $query = "SELECT * from brands";
         return $this->db->query($query)->result_array();
     }
 
     // load all categories from the table categories
     function get_all_categories() {
-        $query = "Select * from categories";
+        $query = "SELECT * from categories";
         return $this->db->query($query)->result_array();
     }
 
     // load all items from the table items
     function get_all_items() {
-        $query = "Select * from items";
+        $query = "SELECT * FROM items";
         return $this->db->query($query)->result_array();
     }
 
     // add new brand to the table brands
     function add_item($data,$file_name) {
 
-        $query = "INSERT INTO items (name,description,brand_id,category_id,price,qty,image,created_at) VALUES (?,?,?,?,?,?,?,?)";
+        $query = "INSERT INTO items (name,description,brand_id,category_id,price,qty,qty_sold,image,created_at) 
+                    VALUES (?,?,?,?,?,?,?,?,?)";
         $values = array(
                     $this->security->xss_clean($data['name']), 
                     $this->security->xss_clean($data['description']),
                     $this->security->xss_clean($data['brand']), 
                     $this->security->xss_clean($data['category']),
                     $this->security->xss_clean($data['price']),
-                    $this->security->xss_clean($data['quantity']),  
+                    $this->security->xss_clean($data['quantity']), 
+                    $this->security->xss_clean(0),   
                     $this->security->xss_clean($file_name),   
                     date("Y-m-d h:i:s")
         ); 
@@ -62,7 +64,7 @@ class Product_Model extends CI_Model {
     }
 
     // updating the selected category in the database
-    function update_category($data){
+    function update_category($data) {
         $query = "UPDATE categories SET category = ? ,
                                         updated_at = ? 
                                         WHERE id = ?";
@@ -76,11 +78,10 @@ class Product_Model extends CI_Model {
     }
 
     // updating selected brand in the database
-    function update_brand($data){
+    function update_brand($data) {
         $query = "UPDATE brands SET brand = ? ,
-                                        updated_at = ? 
-                                        WHERE id = ?";
-                                    
+                    updated_at = ? 
+                    WHERE id = ?";                      
         $values = array(
                     $this->security->xss_clean($data['brand']),
                     date("Y-m-d h:i:s"),
@@ -89,33 +90,48 @@ class Product_Model extends CI_Model {
         return $this->db->query($query,$values);
     }
 
-    function delete_category($id){
+    function delete_category($id) {
         $query = "DELETE FROM categories WHERE id = ?";                           
         $values = array($this->security->xss_clean($id));
         return $this->db->query($query,$values);
     }
 
-    function delete_brand($id){
+    function delete_brand($id) {
         $query = "DELETE FROM brands WHERE id = ?";                           
         $values = array($this->security->xss_clean($id));
         return $this->db->query($query,$values);
     }
 
-    function get_items($limit,$start,$where){
+    function get_items($limit,$start,$where) {
         $this->db->select('items.id, name, description,price,qty,image,categories.category, brands.brand');
         $this->db->from('items');
         $this->db->join('categories', 'items.category_id = categories.id', 'left');
         $this->db->join('brands', ' items.brand_id = brands.id', 'left');
         $this->db->where($where);
+        // $this->db->or_like('category', $search);
         $this->db->limit($limit,$start);
         $query = $this->db->get_compiled_select();
-        
         // return $this->db->where($where)->limit($limit,$start)->get('items')->result_array();
         return $this->db->query($query)->result_array();
     }
 
-    function count_items($where){
+    function count_items($where) {
+        // $this->db->select('items.id, name, description,price,qty,image,categories.category, brands.brand');
+        // $this->db->from('items');
+        // $this->db->join('categories', 'items.category_id = categories.id', 'left');
+        // $this->db->join('brands', ' items.brand_id = brands.id', 'left');
+        // // $this->db->where($where);
+        // $this->db->like('name', $search);
+        // $this->db->or_like('category', $search);
+        // $count_reponse  = $this->db->count_all_results();
+        // return $count_reponse;
+        // return $this->db->join('categories', 'items.category_id = categories.id', 'left')->where($where)->or_like('category', $search)->count_all_results('items');
         return $this->db->where($where)->count_all_results('items');
+    }
+
+    function count_all_items() {
+        $query = "SELECT * FROM items";
+        return $this->db->query($query)->result_array();
     }
 
     function get_item_by_id($id) {
